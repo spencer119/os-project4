945a67c36dd9e38136ec9342c4b82b1a5941f2b0
Spencer <spencernhamilton@gmail.com>
2024-04-23 11:50:32 -0600

    testing

--- src/algorithms/mlfq/mlfq_algorithm.cpp
+++ src/algorithms/mlfq/mlfq_algorithm.cpp
@@ -19,40 +19,7 @@ MLFQScheduler::MLFQScheduler(int slice) {
 std::shared_ptr<SchedulingDecision> MLFQScheduler::get_next_thread() {
     auto decision = std::make_shared<SchedulingDecision>();
 
-    if (queues.empty()) {
-        decision->time_slice = -1;
-        decision->thread = nullptr;
-        decision->explanation = "No threads available for scheduling.";
-        // return decision;
-    } else {
-        for (int i = 0; i < n; i++) {
-            while (!queues[i].empty()) {
-                auto thread = queues[i].top();
-                queues[i].pop();
-                int current_slice = pow(2, i);
-                // std::cout << "Time Slice: " << pow(2, i) << std::endl;
-                if (thread->service_time - thread->time_added_to_queue >= current_slice) {
-                    if (i < n - 1) {
-                        thread->time_added_to_queue = thread->service_time;
-                        queues[i + 1].push(thread->priority, thread);
-                    } else {
-                        decision->thread = thread;
-                        decision->time_slice = pow(2, i);
-                        // Selected from queue Z (priority = P, runtime = R). Will run for at most Y ticks.
-                        decision->explanation = fmt::format("Selected from queue {} (priority = {}, runtime = {}). Will run for at most {} ticks.", i, PROCESS_PRIORITY_MAP[thread->priority], thread->service_time - thread->time_added_to_queue, decision->time_slice);
-                        return decision;
-                    }
-                } else {
-                    decision->thread = thread;
-                    decision->time_slice = pow(2, i);
-                    // Selected from queue Z (priority = P, runtime = R). Will run for at most Y ticks.
-                    decision->explanation = fmt::format("Selected from queue {} (priority = {}, runtime = {}). Will run for at most {} ticks.", i, PROCESS_PRIORITY_MAP[thread->priority], thread->service_time - thread->time_added_to_queue, decision->time_slice);
-                    return decision;
-                }
-            }
-        }
-    }
-    return decision;
+       return decision;
 }
 // for (int i = 0; i < n; i++) {
 //     if (!queues[i].empty()) {
